05a52ae49edde886b1c9d06d3bb3e160b575d499
Ahnaf Alfariza <ahnafalfariza@outlook.com>
2021-02-03 13:19:29 +0700

    setup router and graphql

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { PokemonProvider } from "./context/PokemonContext";
+
+import MyPokemon from "./page/MyPokemon";
+import PokemonDetail from "./page/Pokemon";
+import PokemonList from "./page/PokemonList";
+import Nav from "./component/Nav";
+
+function PokemonRoute() {
+  return (
+    <Router>
+      <Nav />
+      <Switch>
+        <Route path="/mypokemon">
+          <MyPokemon />
+        </Route>
+        <Route path="/:pokemon">
+          <PokemonDetail />
+        </Route>
+        <Route path="/">
+          <PokemonList />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <PokemonProvider value={[]}>
+      <PokemonRoute />
+    </PokemonProvider>
   );
 }
 
